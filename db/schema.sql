e5de2d6798af79211ae140014d8eb00ecb4619c6
Andri Möll <andri@dot.ee>
2020-09-25 22:19:34 +0000

    Add database indices.

--- db/schema.sql
+++ db/schema.sql
@@ -168,6 +168,22 @@ CREATE TABLE political_party_members (
 	CONSTRAINT joined_on_format
 	CHECK (joined_on GLOB '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]')
 );
+CREATE INDEX index_procurements_on_country
+ON procurements (country);
+CREATE INDEX index_procurements_on_buyer
+ON procurements (buyer_country, buyer_id);
+CREATE INDEX index_procurement_contracts_on_procurement
+ON procurement_contracts (procurement_country, procurement_id);
+CREATE INDEX index_procurement_contracts_on_seller
+ON procurement_contracts (seller_country, seller_id);
+CREATE INDEX index_organization_people_on_organization
+ON organization_people (organization_country, organization_id);
+CREATE INDEX index_organization_people_on_person
+ON organization_people (person_country, person_id);
+CREATE INDEX index_political_party_donations_on_donator
+ON political_party_donations (donator_normalized_name, donator_birthdate);
+CREATE INDEX index_political_party_members_on_normalized_name_and_birthdate
+ON political_party_members (normalized_name, birthdate);
 
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
@@ -184,4 +200,5 @@ INSERT INTO migrations VALUES('20200914222117');
 INSERT INTO migrations VALUES('20200923160352');
 INSERT INTO migrations VALUES('20200924211656');
 INSERT INTO migrations VALUES('20200925162535');
+INSERT INTO migrations VALUES('20200925213326');
 COMMIT;
