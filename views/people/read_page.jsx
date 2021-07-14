1843f3b155f271f071480cdd62c8a004cafb7f34
Andri Möll <andri@dot.ee>
2021-07-14 08:16:06 +0000

    Permit hiding everyone's birthday.

--- views/people/read_page.jsx
+++ views/people/read_page.jsx
@@ -14,7 +14,7 @@ var {MoneyElement} = Page
 var Config = require("root/config")
 var {RolesTable} = require("../organizations/read_page")
 var ROLES = require("root/lib/procurement").ORGANIZATION_ROLES
-var HIDDEN_BIRTHDAYS = new Set(Config.hiddenBirthdays)
+var SHOW_BIRTHDAYS = Config.showBirthdays
 
 module.exports = function(attrs) {
 	var {person} = attrs
@@ -28,10 +28,7 @@ module.exports = function(attrs) {
 			<p class="header-subtitle birthdate">
 				<FlagElement country={person.country} />
 
-				{(
-					person.birthdate &&
-					!HIDDEN_BIRTHDAYS.has(person.country + person.personal_id)
-				) ? <Fragment>
+				{person.birthdate && SHOW_BIRTHDAYS ? <Fragment>
 					Born <DateElement at={person.birthdate} />
 				</Fragment> : null}
 			</p>
